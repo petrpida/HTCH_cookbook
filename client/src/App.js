d48df563fd449ddde629b056311f36eae11d1b67
Petr Studeny <petr.studeny@unicorn.com>
2022-09-13 11:08:20 +0200

    homepage update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const title = "Cookbook App"
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{title}</h1>
+        <div className='box'>
+            <p>Welcome to my very first {title} made with React!</p>
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
     </div>
   );
 }
